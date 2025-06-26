e1fa3a03a5ac503d41cfe7ceff2700a995fc00b2
sanju <sanjukumbar408@gmail.com>
2025-06-27 01:21:33 +0530

    Add files via upload

---
+++ filter.js
@@ -0,0 +1,407 @@
+const data = [
+    {
+        id: 1,
+        name: "Invicta women's pro Diver",
+        img: "https://www.bbassets.com/media/uploads/p/l/40335057_1-bolly-lounge-girls-blush-pink-floral-dress.jpg",
+        price: 74,
+        cat: "Dress",
+    },
+    {
+        id: 2,
+        name: "Invicta Men's pro Diver",
+        img: "https://www.casio.com/content/dam/casio/product-info/locales/in/en/timepiece/product/watch/G/GA/GA2/ga-2100mng-7a/assets/GA-2100MNG-7A.png.transform/main-visual-sp/image.png",
+        price: 150,
+        cat: "Luxury",
+    },
+    {
+        id: 3,
+        name: "Nivia Storm Football - Size 5 (White), Rubber",
+        img: "https://m.media-amazon.com/images/I/61RpRYFb2wL._SX679_.jpg",
+        price: 100,
+        cat: "Sport",
+    },
+    
+    {
+        id: 4,
+        name: "Titan Men's Analog Watch",
+        img: "https://m.media-amazon.com/images/I/6137Q5gE6VL._SX679_.jpg",
+        price: 120,
+        cat: "Luxury",
+    },
+    
+    {
+        id: 5,
+        name: "FINIVO FASHION Men's  Casual Shirt",
+        img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRGWMF3OnPSp-JvW-1uPj9j34RWHzpc4iOStHsX0zgiU3EX8_rk1Pbh4NmO-k9id8c8SfvJ6j0TRYIinvLeKNusqbFemYRv_3RE6qk6YYJr5fKfpMK7GO5_AdmfsmRcwwDMamYOl4I&usqp=CAc",
+        price: 80,
+        cat: " men's Casual",
+    },
+    
+    {
+        id: 6,
+        name: "Nivia Storm Football Shoes for Men/Sports",
+        img: "https://m.media-amazon.com/images/I/716yLjHsrZL._SY695_.jpg",
+        price: 60,
+        cat: "Sport",
+    },
+
+    
+    {
+        id: 6,
+        name: "Timex Men's Analog Watch",
+        img: "https://m.media-amazon.com/images/I/61jigCEYg-L._SX679_.jpg",
+        price: 54,
+        cat: "Luxury",
+    },
+    {
+        id: 7,
+        name: "Invicta women's Diver",
+        img: "https://www.bbassets.com/media/uploads/p/l/40335110_1-bolly-lounge-girls-snow-white-pink-floral-dress.jpg",
+        price: 90,
+        cat: "Dress",
+    },
+    
+   
+    
+    {
+        id: 9,
+        name: "HF MRF GRAND VK-18 Set of Complete Cricket Kit",
+        img: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTYt3RPP0i3VSlgRQpDYHXuILgy7blmtTTPE1ukEJH1eJ6YxwvVvU_I8AR89zhtvmLZqTQ1fWq8j_ECghhZWsHq-zYYfwjrAausoXMy6Aojx32MB7A_xDy6k-J1ussKNaB9mQ0j_EtiJw&usqp=CAc",
+        price: 80,
+        cat: "Sport",
+    },
+    {
+        id: 10,
+        name: "Invicta women's pro Diver",
+        img: "https://www.bbassets.com/media/uploads/p/l/40335070_1-bolly-lounge-girls-snow-white-pink-floral-dress.jpg",
+        price: 200,
+        cat: "Dress",
+    },
+     {
+        id: 11,
+        name: "The Roadster Lifestyle Co Men's Solid Regular Fit",
+        img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRDpO18dXY7Z3WdNIud1jFn7UkoMUvME7FEPFNneAfxxRhdlPq8xXNwvZpVsVv0XSg_1LYW9zV3VwCOv_NWPpcbKvJyWnYgKKAISLyXxSbd",
+        price: 170,
+        cat: " men's Casual",
+    },
+     {
+        id: 12,
+        name: "FINIVO FASHION Men's Textured Casual Shirt",
+        img: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQAqX8FmFQVy_UPlF1yKTTnfsRgrKSDRScgh9OgAcgYaGjbbGELowVtyI6M3Pkeh9zBKnAtnjWe9L9FtYNyxjW5rX4g43nXgR1waOn8o9b8MrH4eIVO9FDA",
+        price: 80,
+        cat: " men's Casual",
+    },
+     {
+        id: 13,
+        name: "The Roadster Lifestyle Co Men's Solid Regular Fit",
+        img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTdrEVHrkx4EDqbMCQziH71OjGUw2HDdZx5ckYPxRz36PV5NDkEVx-exvi1RxVHOV-vQlWUTEgg_Fv4mgwJYkNjGGMzyYozrLEIgvyM1CHE",
+        price: 145,
+        cat: " men's Casual",
+    },
+      {
+        id: 14,
+        name: "The Roadster Lifestyle Co Men's Solid Regular Fit",
+        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcS0udEc-1pkQd9IuiDlY1FAcgu0y7PqnjORcb7mXa3TLAKhhYnxemV4k8eRyB30nC_qPYcdL2W79G1yAFFPiHg_SwAM0Kl2RQxgepDlq97wkSM-wj9_BBz3JQ",
+        price: 145,
+        cat: " men's Casual",
+    },
+     {
+        id: 15,
+        name: "Timex Men's Analog Watch",
+        img: "https://www.sonatawatches.in/dw/image/v2/BKDD_PRD/on/demandware.static/-/Sites-titan-master-catalog/default/dw5106d058/images/Sonata/Catalog/77070PP05_1.jpg?sw=600&sh=600",
+        price: 54,
+        cat: "Luxury",
+    },
+     {
+        id: 16,
+        name: "Evolve Pro",
+        img: "https://skmei.in/cdn/shop/files/1688color1_2_412b87a3-6511-48a4-9ef3-e35b09520336.jpg?v=1749208089&width=823",
+        price: 54,
+        cat: "Luxury",
+    },
+    {
+        id: 17,
+        name: "Invicta women's pro Diver",
+        img: "https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/catalog/product/f/8/f87f706964397_1.jpg?rnd=20200526195200&tr=w-1080",
+        price: 200,
+        cat: "Dress",
+        
+    },
+    { id: 18,
+        name: "Perfly By Decathlon Adult Badminton Racket Br100 Set",
+        img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTJKobuTppIAssmdn1beBxyVuAM-3FBrj6wZuH-B7FUuqhmN1y1w5UN9ml_W15tmMIyUvOfJ_yhGU13U84y2XzULsGGSsIYOUAn7ySwduykUnBDaoCr3gqoQg",
+        price: 160,
+        cat: "Sport",
+    },
+     { 
+        id: 19,
+        name: "Quechua by Decathlon Men Dark Green 20L Hiking Backpack",
+        img: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTtKkoovuSBzejmnwmN9_VUqDKkko5dWdNhWQgZjA8E23aDtPM52gdWafVUtpokEs32zWC8iXhbVlej-qHxo4Ra4JUsFMb4KJP7b3VrBVgjfDpQ6rIWnUoN",
+        price: 120,
+        cat: "Sport",
+    },
+      
+    {
+        id: 5,
+        name: "The Souled Store Men's Solid Casual Shirt",
+        img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTF8I418jQr1lCeO9_lLDbIUjnlRGwDI7ar7JGXTr0uvSLpttIQ7bu0TQYBE4k2vBpMM2zKV86DVS0VZpjhXAc1r53HzjCIV88-go-YJO-oVyfnqWD1vYM2Yg",
+        price: 110,
+        cat: " men's Casual",
+    },
+      
+    {
+        id: 5,
+        name: "NexaFlair Casual Shirt for Men| Shirt for Men",
+        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT5TyuD0ITIupgWJv7B9s8YmH5FamtinsOxNsoGn_lHVnpnq5s5ByVmWfDjvtfbVsGRgSztxGloDRocvcBZUATczl7gjVBNPpJLOC9L9rrnaL1F6VqsBUWDFQ",
+        price: 75,
+        cat: " men's Casual",
+    },
+    
+  {
+        id: 17,
+        name: "LookShine Dress For Women  Beautiful",
+        img: "https://images.meesho.com/images/products/449712649/ccjop_1200.jpg",
+        price: 170,
+        cat: "Dress",
+        
+    },
+       
+  {
+        id: 17,
+        name: "Leriya Fashion Women's Western Midi Party Dress",
+        img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTeQEMAroGKTN2MRPoM49tbbEl625LQkideSvE9KJUgIdKllFH1nwBYMD29dmy4Y3TQLOwIdkRgXMjUIruu-nZJ2sZQCiwEVZpGBnxlI72ruLr-jRX2lRYGfw",
+        price: 220,
+        cat: "Dress",
+        
+    },
+    {
+        id: 17,
+        name: "Leriya Fashion Women's Western Midi Party Dress",
+        img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSzO9nwaS9F6jyyHI4so8Ojs4RLm9XTYOk4ge1e3uN6rWllmfkJ4chyaHpXkrX5IJi4cJppoZ9dgOaM38W84xdup47khpijNpLLoQ76msKbUWzIRTMiX9EFww",
+        price: 210,
+        cat: "Dress",
+        
+    },
+     {
+        id: 16,
+        name: "EXCALIBUR SPIDER HURACAN PINK GOLD 45MM",
+        img: "https://img.tatacliq.com/images/i10/658Wx734H/MP000000016546488_658Wx734H_202302211423421.jpeg",
+        price: 94,
+        cat: "Luxury",
+    },
+     {
+        id: 16,
+        name: "Fastrack Unisex-Adult Analog Grey Dial Watch",
+        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQaw-hVqL0I2ocaWfyGLXISAsbdLjyjl3PBHMnhQ4mAlHndO6NigY-GlIy4DQYYqOYSeCxmdWz0j_cSR7R5QvSt7jW3nK_2YSAp-BESK4KPpvMATN484DvY",
+        price: 54,
+        cat: "Luxury",
+    },
+      {
+        id: 9,
+        name: "Protoner PVC DM Dumbbells Set and Fitness Kit",
+        img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSjghBS-dm8Ddp54dnC55zJVGaQR8fb6ClvDCbIdIwPx9cieSpNwGJrp_-8PzX7zp1Cn9jdOMAUWHGZv9qTGQaCnjPxKfyCPmrKIyS0w-yExNvt8bLSfuv2",
+        price: 160,
+        cat: "Sport",
+    },
+      {
+        id: 9,
+        name: "FIFA Basic Football Club Hybrid",
+        img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTvldpOA_OFLq_as549QL_MqjpSIqFL7N4fdPpaB6nLurWUkV1lTFoaRvGFBEuqB5mMflkjHiN5GUJYCkviic1ErO_F1CZ6MhQmwVTBloUVjvK-fLcrkkoW0g",
+        price: 100,
+        cat: "Sport",
+    },
+     {
+        id: 2,
+        name: "HP Laptop 39.6 cm (15.6) , Silver",
+        img: "https://m.media-amazon.com/images/I/61zegicE0CL._SX679_.jpg",
+        price: 250,
+        cat: "Electronics",
+    },
+      {
+        id: 2,
+        name: "Google Pixel 9 Pro XL 5G",
+        img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRuFPnjrgIAmXA_zHLcMk_6LWgjKZAL-9vOsfmMe466DbMihCzmwK92ed6GpBXfsfQS8XQ_5oPxkaTmOBwSQ8PaOkvbDKjqPWCj7cPDWusDTrKrnD9Rdj4icD6M3JrZSh5xll-XvEM&usqp=CAc",
+        price: 350,
+        cat: "Electronics",
+    },
+    {
+        id: 2,
+        name: "Sony  ZV-E10L 24.2 MP Interchangeable-Lens",
+        img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQEWvag66PIhEseq4qvqwJryQ0kA0qf1YU7WsaxGJnmhsSGZYnEyjJzCgzyAD4AsrvVP4HJ394EFvVFnuoHBM2h-TgJdEp0DI1JhKGuv91JzKGVhK9PfGkAqAVIvvgOOwtQUC6kRgI3I4s&usqp=CAc",
+        price: 360,
+        cat: "Electronics",
+    },
+     {
+        id: 2,
+        name: "Lenovo Intel Celeron N4020 Thin Light Laptop",
+        img: "https://rukminim2.flixcart.com/image/1200/1200/xif0q/computer/e/i/s/-original-imahcd9pwzrzkhg2.jpeg",
+        price: 250,
+        cat: "Electronics",
+    },
+     {
+        id: 2,
+        name: "KILBURN II",
+        img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSamJz1TGs9N0Gi23dOGhL82rG_yfW4fw755A8_8UcUo_94xGaalK1mCN8-8ImtJ9gZSwT8q26A4bIFKSCL7Ne46QCFeTzu_fgSLHzCbO10seN-mz_GKCDF1cUA90ZcJqJVbReEyw&usqp=CAc",
+        price: 300,
+        cat: "Electronics",
+    },
+      {
+        id: 2,
+        name: "Sony New SA-D40M2 4.1ch Home Theatre",
+        img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTVogABgeNBIhAxZ2GKyBu4N87s4VQcVQ7HdP1gH6-EwKpMtTVsJmP1xMpklQi9UqjWEksipcjP2wc1CB04sluo5w327964uD_aGOLI0l-xat92rVRrtVj8mKqaZf2ZYkyCE0WS2skOy6Q&usqp=CAc",
+        price: 210,
+        cat: "Electronics",
+    },
+     
+    {
+        id: 2,
+        name: "1.08 m (43) QLED QEF1 4K Samsung",
+        img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQ7ijzt02LnZoUXaZizGjyrimxfOcb3p-8WQtSxHZMYFIg52rQYeuFSEQHQIO1tMRd4-LFFP3zJ6wTPz8TaPWG-8YXv_o34WZrsNHR5Vc83nt3lsE3l5TFlE7TUk0YVAzD-VbZBW0mMziE&usqp=CAc",
+        price: 370,
+        cat: "Electronics",
+    },
+      {
+        id: 2,
+        name: "Samsung 138 cm (55 Inch) 4K  Smart TV",
+        img: "https://cdn.jiostore.online/v2/jmd-asp/jdprod/wrkr/products/pictures/item/free/resize-w:450/C2x5BTK9ZG-samsung-55q60d-led-tv-494410289-i-1-1200wx1200h.jpeg",
+        price: 330,
+        cat: "Electronics",
+    },
+     {
+        id: 2,
+        name: "Canon EOS R50 RF-S18-45mm f/4.5-6.3 ",
+        img: "https://m.media-amazon.com/images/I/81LskAU5h1L._SL1500_.jpg",
+        price: 210,
+        cat: "Electronics",
+    },
+    {
+        id: 2,
+        name: "EXNBS02 Black View Side Mirrors",
+        img: "https://static1.industrybuying.com/products/automotive-maintenance-and-accessories/2-wheeler-accessories/bike-accessories/AUT.BIK.69794112_1686905621059.webp",
+        price: 120,
+        cat: "Auto acc",
+    },
+     {
+        id: 2,
+        name: "Steelbird SBH-57 Fighter ISI and DOT Certified ",
+        img: "https://m.media-amazon.com/images/I/61wjiokz2BL._SX679_.jpg",
+        price: 245,
+        cat: "Auto acc",
+    },
+     {
+        id: 2,
+        name: " DETACHI Car Door Handle Protector",
+        img: "https://m.media-amazon.com/images/I/518ArBys9yL._AC_UL480_FMwebp_QL65_.jpg",
+        price: 200,
+        cat: "Auto acc",
+    },
+       {
+        id: 2,
+        name: " OTOROYS Mini Driving Fog Light Lamp Projector",
+        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTT4eFDAKdgMs_wdjcpO7gor_mA7rZNcPt29tR4h6iktpzBD0WJ3YPesf3c8uLZuMXWuA6CGTzOL3ioHCTFtbQdjZU3Y4d5ekAF6UJnth0xXoPoI6WWtZaW",
+        price: 210,
+        cat: "Auto acc",
+    },
+       {
+        id: 2,
+        name: " AutoThrill Premium Metal Key Cover",
+        img: "https://m.media-amazon.com/images/I/61imn+tIgIL._SY879_.jpg",
+        price: 220,
+        cat: "Auto acc",
+    },
+       {
+        id: 2,
+        name: " Sounce Portable High Pressure Foot Air Pump",
+        img: "https://m.media-amazon.com/images/I/71VTvhCOyzL._AC_UL480_FMwebp_QL65_.jpg",
+        price: 260,
+        cat: "Auto acc",
+    },
+       {
+        id: 2,
+        name: "Frido Ultimate Car Comfort Combo | ",
+        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTgohp5opD8x0j87FOgGyuiYnATC8W3rXNgcqeIR3x6QJwmG0Id0c0hTR07jOWcKzfqgulQKY2nHYzFjq40d7sYGKNtv6ZKasjlqfXUo9bP0ubun75SQ51i",
+        price: 290,
+        cat: "Auto acc",
+    },
+];
+
+
+
+
+
+const productsContainer = document.querySelector(".product")
+const searchInput = document.querySelector(".search")
+const categoriesContainer = document.querySelector(".cats")
+const priceRange = document.querySelector(".priceRange")
+const priceValue = document.querySelector(".priceValue")
+
+const displayProducts = (filteredProducts) =>{
+    productsContainer.innerHTML = filteredProducts.map(products=>
+        `
+         <div class="products">
+                <img src=${products.img}
+                <span class="name">${products.name}</span>
+                <span class="priceText">${products.price}</span>
+            </div>
+    
+        `
+    ).join("");
+};
+
+displayProducts(data)
+
+searchInput.addEventListener("keyup",(e) => {
+    //console.log(e.target.value)
+    const value = e.target.value.toLowerCase();
+
+    if(value){
+
+        displayProducts(data.filter(item=> item.name.toLowerCase().indexOf(value) !== -1))
+
+    }else{
+        displayProducts(data);
+    }
+
+});
+
+const setCategories = () =>{
+const allCats = data.map(item=>item.cat)
+const Categories =[
+    "All",
+    ...allCats.filter((item,i)=>{
+    return allCats.indexOf(item)===i
+}),
+];
+categoriesContainer.innerHTML = Categories.map(cat=>
+    `
+    <span class="cat">${cat}</span>
+    `).join("")
+
+    categoriesContainer.addEventListener("click", (e)=> {
+        const selectedCat = e.target.textContent;
+
+        selectedCat ==="All"
+        ? displayProducts(data)
+        :displayProducts(data.filter((item)=> item.cat === selectedCat));
+    })
+};
+
+const setPrices  = ()=>{
+    const priceList = data.map(item=>item.price);
+    const minPrice= Math.min(...priceList)
+    const maxPrice= Math.max(...priceList)
+
+    priceRange.min = minPrice
+    priceRange.max = maxPrice
+    priceRange.value=maxPrice
+    priceValue.textContent = "$" + maxPrice
+
+    priceRange.addEventListener("input",(e)=>{
+        priceValue.textContent = "$" + e.target.value;
+        displayProducts(data.filter((item) => item.price <= e.target.value));
+    });
+
+
+};
+setCategories();
+setPrices();
